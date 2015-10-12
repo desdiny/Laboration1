18482a3d8dd1c98c7be7d447f99d05bef424a3c8
Trolle <trolle.geuna@gmail.com>
2015-10-12 18:26:04 +0200

    startat lagga till de olika deduktionsbevisen

--- laboration1.pl
+++ laboration1.pl
@@ -1,27 +1,52 @@
 % Laboration 1 Logik för dataloger, Beviskontroll med Prolog
 
+% ta in inputet
+verify(InputFileName):-
+  see(InputFileName),
+  read(Perms),read(Goal), read(Proof),
+  seen,
+  valid_proof(Perms, Goal, Proof).
+
+% ---------------------------------------------------
 %Vi kommer behöva alla dedkutions bevisen
 
-% AND ELIMINATION
-
+% AND ELIMINATION 1
+valid_proof(Premisser, [Rad, Resultat, andel1(X)], Valid):-
+  member([X, and(_ , Resultat), _], Valid).
+% AND ELIMINATION 2
+valid_proof(Premisser, [Rad, Resultat, andel2(X), Valid]):-
+  member([X, and(_, Resultat, _), Valid]).
 % AND INTRODUCTION
-
-% OR ELIMINATION
-
+valid_proof()
+% OR ELIMINATION 1
+valid_proof()
+% OR ELIMINATION 2
+valid_proof()
 % OR INTRODUCTION
-
+valid_proof()
 % IMPLICATION ELIMINATION
-
-% IMPLICATION INTRODUCTION 
-
+valid_proof()
+% IMPLICATION INTRODUCTION
+valid_proof()
 % NEGATION ELIMINATION
-
+valid_proof()
 % NEGATION INTRODUCTION
-
+valid_proof()
 % CONTRADICTION ELIMINATION
-
+valid_proof()
 % DOUBLE NEGATION ELIMINATION
+valid_proof()
 
 % COPY
+valid_proof(Premisser, [Rad, Resultat, copy(X)], Valid):-
+  member([X, Resultat, _], Valid).
+% MT?
 
+% DOUBLE NEGATION INTRODUCTION ?
 
+% PBC ??
+
+% LEM ??
+% ---------------------------------------------------
+
+% Boxhantering
